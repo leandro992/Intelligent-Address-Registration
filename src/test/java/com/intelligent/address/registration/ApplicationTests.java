8b7775cc3493756198a33ab529ed60c8898e08cf
victorleandro <victor.leandro-terceiro@cea.com.br>
2025-07-13 11:22:17 +1000

    project creation

---
+++ src/test/java/com/intelligent/address/registration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligent.address.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
